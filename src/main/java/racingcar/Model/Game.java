7899db9131402fa5e1868304c0390a5b2852f591
구동엽 <ehd_duq94@naver.com>
2023-11-26 16:17:35 +0900

    refactor : 자동차 입력 알고리즘 리팩토링

--- src/main/java/racingcar/Model/Game.java
+++ src/main/java/racingcar/Model/Game.java
@@ -2,41 +2,30 @@ package racingcar.Model;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.Service.Exception1;
+import racingcar.Service.SplitCarName;
 import racingcar.View.GameView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Game {
     Exception1 exception = new Exception1();
-    GameView gameView = new GameView();
-
-    private List<Car> cars;
-    private int Attempt;
-
-    public Game(){
-
-    }
-
-    public Game(List<Car> cars, int Attempt){
-        this.cars = cars;
-        this.Attempt = Attempt;
-    }
+    static GameView gameView = new GameView();
 
     public void Initialize(){
         String CarName = InputCarName();
         exception.CarIsNull(CarName);
-        this.Attempt = InputAttempts();
+        int Attempt = InputAttempts();
         exception.ValidateAttempts(Attempt);
+        new RacingCars(SplitCarName.CreateCar(CarName),Attempt);
     }
 
     public static String InputCarName(){
+        gameView.ShowGameStart();
         return Console.readLine();
     }
 
     public static int InputAttempts(){
+        gameView.ShowTryNumber();
         return Integer.parseInt(Console.readLine());
     }
 
+    // public List<Car> SplitCarName(String InputCarName)
 }
