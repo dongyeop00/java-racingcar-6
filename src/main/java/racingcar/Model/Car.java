5736f4c49523c35ebb97764ba86d72dc7f5ae426
구동엽 <ehd_duq94@naver.com>
2023-11-28 02:10:54 +0900

    refactor : 랜덤값에 의해 움직이는 함수 구현

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -3,11 +3,11 @@ package racingcar.Model;
 import racingcar.Service.Exception1;
 
 public class Car {
-    private final String name;
+    private String name;
+
     private int position = 0;
 
     public Car(String name){
-        new Exception1().ValidateName(name); //5글자 이하인지 예외처리
         this.name = name;
     }
 
@@ -17,6 +17,16 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public void ToMove(int RandomNumber){
+        if(RandomNumber >= 4){
+            position++;
+        }
+    }
+
+    public String ShowRound(){
+        return this.name + " : " + "-".repeat(position);
     }
 
 }
